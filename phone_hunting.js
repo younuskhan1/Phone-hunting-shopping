545d93a29f002a6f45a8b01f52891beac2f8cebc
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-27 20:38:11 +0600

    work of show all button is running

--- phone_hunting.js
+++ phone_hunting.js
@@ -21,6 +21,7 @@ const loadPhoneDataBySearch = async () => {
         const response = await fetch(url);
         const data = await response.json();
         showingPhones(data.data);
+        // console.log(dataLimit);
     }
     catch (error) {
         console.log(error);
@@ -29,32 +30,38 @@ const loadPhoneDataBySearch = async () => {
 
 const showingPhones = (phones) => {
     // console.log(phones)
+
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.innerHTML = "";
     const noPhoneFound = document.getElementById("no-phone-found");
+    const showAllButton = document.getElementById("show-all-button");
 
     if (phones.length === 0) {
         noPhoneFound.classList.remove("hidden");
     } else {
-        for (let phone of phones) {
-            // console.log(phone);
-            const div = document.createElement("div");
-            div.classList = `card bg-base-100 shadow-xl rounded-none`;
-            div.innerHTML = ` 
-                <figure class="px-2 m-3 py-4 bg-[#deeff5] rounded-none">
-                    <img class="w-4/5 rounded-3xl" src="${phone.image}" alt="Shoes" class="rounded-xl" />
-                </figure>
-                <div class="card-body items-center text-center">
-                    <h2 class="card-title">${phone.phone_name}</h2>
-                    <p class="break-all">${phone.slug}</p>
-                    <p>${phone.brand}</p>
-                    <div class="card-actions">
-                        <button class="btn btn-secondary" onClick="loadingShowDetailsData('${phone.slug}')">Show Details</button>
+        if (phones.length > 8) {
+            phones = phones.slice(0, 8);
+            showAllButton.classList.remove("hidden");
+            for (let phone of phones) {
+                // console.log(phone);
+                const div = document.createElement("div");
+                div.classList = `card bg-base-100 shadow-xl rounded-none`;
+                div.innerHTML = ` 
+                    <figure class="px-2 m-3 py-4 bg-[#deeff5] rounded-none">
+                        <img class="w-4/5 rounded-3xl" src="${phone.image}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title">${phone.phone_name}</h2>
+                        <p class="break-all">${phone.slug}</p>
+                        <p>${phone.brand}</p>
+                        <div class="card-actions">
+                            <button class="btn btn-secondary" onClick="loadingShowDetailsData('${phone.slug}')">Show Details</button>
+                        </div>
                     </div>
-                </div>
-            `;
-            phoneContainer.appendChild(div);
-            noPhoneFound.classList.add("hidden");
+                `;
+                phoneContainer.appendChild(div);
+                noPhoneFound.classList.add("hidden");
+            }
         }
     }
     contentIsLoading(false);
@@ -78,14 +85,13 @@ const displayShowDetails = (phone) => {
     showDetailsContainer.innerHTML = `
         <dialog id="my_modal_5" class="modal">
         <div class="modal-box">
-            <div class="flex justify-center mb-4"><img class="w-2/5" src="${phone.image}" alt="Shoes" class="rounded-xl" /></div>
+            <div class="flex justify-center mb-4"><img class="md:w-2/5 lg:w-2/5 w-3/5 " src="${phone.image}" alt="Shoes" class="rounded-xl" /></div>
             <h3 class="font-bold text-lg text-center">Phone Name : ${phone.name}</h3>
             <p class="text-center"> Bluetooth : ${phone.others ? phone.others.Bluetooth : "Bluetooth is not available"}</p>
             <p class="text-center"> Release Date : ${phone.releaseDate ? phone.releaseDate : "Release Date not available"}</p>
             <p class="text-center"> GPS : ${phone.others ? phone.others.GPS : "GPS is not available"}</p>
             <p class="text-center"> Storage : ${phone.mainFeatures.storage}</p>
             <p class="text-center"> WLAN : ${phone.others ? phone.others.WLAN : "WLAN is not available"}</p>
-
             <form method="dialog">
                <div class="flex justify-center mt-4"><button class="btn btn-primary">Close the Modal</button></div>
             </form>
@@ -96,8 +102,9 @@ const displayShowDetails = (phone) => {
 
 }
 
-
-
+const showAllButtonHandler = () => {
+    loadPhoneDataBySearch();
+}
 
 const contentIsLoading = (isLoading) => {
     const spinnerContainer = document.getElementById("spinner");
