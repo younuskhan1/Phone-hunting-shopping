6d4d5d3f469dfcbb4b56eba6b5b1db1f09b4f292
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-27 21:46:12 +0600

    the project is finished

--- phone_hunting.js
+++ phone_hunting.js
@@ -11,7 +11,7 @@ const byDefaultPhoneShow = async () => {
 }
 
 const loadPhoneDataBySearch = async (dataRange) => {
-    console.log(dataRange);
+    // console.log(dataRange);
     contentIsLoading(true);
     const inputField = document.getElementById("input-field");
     const inputName = inputField.value;
@@ -22,7 +22,7 @@ const loadPhoneDataBySearch = async (dataRange) => {
         const response = await fetch(url);
         const data = await response.json();
         showingPhones(data.data, dataRange);
-        // console.log(dataLimit);
+
     }
     catch (error) {
         console.log(error);
@@ -37,6 +37,10 @@ const showingPhones = (phones, dataRange) => {
     const noPhoneFound = document.getElementById("no-phone-found");
     const showAllButton = document.getElementById("show-all-button");
 
+    // At previous day, I inserted all codes at if block or else block. At this process,
+    // there had been some problems because we could not get access for other times into 
+    // those blocked codes. This is also the most most important point to remember.
+
     if (phones.length === 0) {
         noPhoneFound.classList.remove("hidden");
     } else {
@@ -44,6 +48,12 @@ const showingPhones = (phones, dataRange) => {
     }
 
     if (dataRange && phones.length > 8) {
+
+        // the most most most important point here. 
+        // phones is reassigned. phones is not a variable here.
+        // phones is a parameter here. 
+        // a non variable thing which can be reassigned like a variable declared by let. 
+
         phones = phones.slice(0, 8)
         showAllButton.classList.remove("hidden");
     } else {
@@ -73,8 +83,6 @@ const showingPhones = (phones, dataRange) => {
     contentIsLoading(false);
 }
 
-
-
 const loadingShowDetailsData = async (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     try {
@@ -88,7 +96,7 @@ const loadingShowDetailsData = async (phoneId) => {
 }
 
 const displayShowDetails = (phone) => {
-    console.log(phone);
+    // console.log(phone);
     const showDetailsContainer = document.getElementById("showDetailsContainer");
     showDetailsContainer.innerHTML = `
         <dialog id="my_modal_5" class="modal">
